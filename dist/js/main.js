748d457174d075a8e83a4326638c6b22291e9d5a
VichaPratummaneeTHA <66987801+VichaPratummaneeTHA@users.noreply.github.com>
2020-10-10 14:36:21 +0700

    Initial Commit

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+// ckediter
+
+CKEDITOR.replace( 'editor1' );
+CKEDITOR.replace( 'editor2' );
+CKEDITOR.replace( 'editor3' );
+
+// Get the current year for the cpoy right
+
+$('#year').text(new Date().getFullYear());
